3e1fb277b81894df4ea8c00ce0933c90f39a5749
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2022-05-02 01:02:00 +0530

    navbar info btn addintion

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { useState } from "react";
 import { GrFormClose } from "react-icons/gr";
+import { AiFillInfoCircle } from "react-icons/ai";
 
 function Navbar() {
   const [open, setOpen] = useState(true);
+
   return (
     <>
       <div className="navbar">
@@ -18,14 +20,23 @@ function Navbar() {
               loading="eager"
             />
           </div>
-          {open && (
-            <div className="coming_soon">
-              <p>Full website is coming soon ...</p>
-              {/* <div className="cross" onClick={() => setOpen(false)}>
-                <GrFormClose />
-              </div>{" "} */}
-            </div>
-          )}
+
+          <div className="nav_info">
+            {open && (
+              <div className="coming_soon">
+                <p>Full website is coming soon ...</p>
+                <div className="cross" onClick={() => setOpen(false)}>
+                  <GrFormClose />
+                </div>
+              </div>
+            )}
+
+            {!open && (
+              <div className="open_info_icon" onClick={() => setOpen(true)}>
+                <AiFillInfoCircle />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
